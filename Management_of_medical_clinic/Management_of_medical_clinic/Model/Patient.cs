0200b91a49a8b5248e08f0910e7b06e21b68c4e8
mabrowarski <mabrowarski@gmail.com>
2023-03-25 21:21:04 +0100

    Update Patient and Visit classes

--- Management_of_medical_clinic/Management_of_medical_clinic/Model/Patient.cs
+++ Management_of_medical_clinic/Management_of_medical_clinic/Model/Patient.cs
@@ -2,6 +2,10 @@
 
 namespace Console_Management_of_medical_clinic.Model
 {
+    /// <summary>
+    /// <c>Patient</c> models a patient registered in clinic.
+    /// <c>Visit</c> list contains visits for a patient object.
+    /// </summary>
     public class Patient
     {
         public int PatientId { get; set; }
@@ -9,9 +13,9 @@ namespace Console_Management_of_medical_clinic.Model
         public string LastName { get; set;}
         public string PESEL { get; set; }
         public EnumSex Sex { get; set; }
-        public DateTime BirthDate { get; set; }
+        public DateOnly BirthDate { get; set; }
         public bool IsActive { get; set; }
-        public DateTime LastVistiDate { get; set; }
+        public DateOnly? LastVistiDate; // TODO: Last visit date should be calculated based on Visits list
 
         public List<Visit> Visits { get; set; } = new List<Visit>();
     }
--- Management_of_medical_clinic/Management_of_medical_clinic/Model/Visit.cs
+++ Management_of_medical_clinic/Management_of_medical_clinic/Model/Visit.cs
@@ -1,17 +1,20 @@
 ﻿namespace Console_Management_of_medical_clinic.Model
 {
+    /// <summary>
+    /// <c>Visit</c> models a doctor appointment in clinic.
+    /// </summary>
     public class Visit
     {
         public int VisitId { get; set; }
-        public DateTime VisitDate { get; set; }
-        public DateTime VisitHour { get; set; }
+        public DateOnly VisitDate { get; set; }
+        public TimeOnly VisitHour { get; set; }
         public decimal Cost { get; set; }
 
-        public int EmployeeId { get; set; }
-        public EmployeeModel Employee { get; set; }
+        public int? EmployeeId { get; set; }
+        public EmployeeModel? Employee { get; set; }
 
-        public int PatientId { get; set; }
-        public Patient Patient { get; set; }
+        public int? PatientId { get; set; }
+        public Patient? Patient { get; set; }
 
         // TODO: Create office class and connect to visit class 1:1
         //public int OfficeId { get; set; }
