df450f19a57473bbc08d0e218d9599317d9bc0bd
mabrowarski <mabrowarski@gmail.com>
2023-03-25 12:52:07 +0100

    Create Patient and Visit classes

---
+++ Management_of_medical_clinic/Management_of_medical_clinic/Model/Patient.cs
@@ -0,0 +1,18 @@
+﻿using Console_Management_of_medical_clinic.Data.Enums;
+
+namespace Console_Management_of_medical_clinic.Model
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+        public string PESEL { get; set; }
+        public EnumSex Sex { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime LastVistiDate { get; set; }
+
+        public List<Visit> Visits { get; set; } = new List<Visit>();
+    }
+}
