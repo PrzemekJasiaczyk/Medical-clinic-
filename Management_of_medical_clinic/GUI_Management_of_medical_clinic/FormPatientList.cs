8d630b39e884ecafd946885dd5f76bbefc258e65
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2023-03-26 12:56:09 +0200

    Add FormPatientList

---
+++ Management_of_medical_clinic/GUI_Management_of_medical_clinic/FormPatientList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Management_of_medical_clinic
+{
+    public partial class FormPatientList : Form
+    {
+        public FormPatientList()
+        {
+            InitializeComponent();
+            dataGridViewPatientList.AllowUserToAddRows = false;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxDate_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxPesel_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxName_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormPatientList_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewPatientList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
