f793700585d7571ea97391ff5373b82b7c6ec661
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-04-15 20:57:08 +0200

    Add AddPatientToDataBase to PatientTest

--- Management_of_medical_clinic/MedicalClinicTest/PatientTest.cs
+++ Management_of_medical_clinic/MedicalClinicTest/PatientTest.cs
@@ -1,7 +1,38 @@
-﻿namespace MedicalClinicTest
+﻿using Console_Management_of_medical_clinic.Data;
+using Console_Management_of_medical_clinic.Data.Enums;
+using Console_Management_of_medical_clinic.Model;
+
+namespace MedicalClinicTest
 {
 	public class PatientTest
 	{
+		// Natalia: dodawanie, znajdywanie pacjenta
 
-	}
+		[Fact]
+
+		public void AddPatientToDataBase()
+		{
+			AppDbContext testContext;
+
+			using (InMemoryDbContext inMemoryDbContext = new())
+			{
+				testContext = inMemoryDbContext.CreateTestContext();
+                Patient patient =
+                new Patient()
+                {
+                    FirstName = "Paweł",
+                    LastName = "Wiktorowicz",
+                    PESEL = "99030300191",
+                    Sex = EnumSex.Male,
+                    BirthDate = new DateTime(1999, 3, 3),
+                    IsActive = true,
+                    LastVisitDate = null
+                };
+
+                Patient.AddPatient(patient, testContext);
+                
+
+            }
+		}
+    }
 }
