696eae07e2c0b793de0979dec57d261f1da2c94f
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-04-16 15:26:15 +0200

    Add a test looking for patients in the database

--- Management_of_medical_clinic/MedicalClinicTest/PatientTest.cs
+++ Management_of_medical_clinic/MedicalClinicTest/PatientTest.cs
@@ -16,21 +16,21 @@ namespace MedicalClinicTest
 
 			using (InMemoryDbContext inMemoryDbContext = new())
 			{
-				testContext = inMemoryDbContext.CreateTestContext();
+                testContext = inMemoryDbContext.CreateTestContext();
                 Patient patient =
                 new Patient()
                 {
                     FirstName = "Paweł",
-                    LastName = "Wiktorowicz",
-                    PESEL = "99030300191",
+                    LastName = "Dawid",
+                    PESEL = "45010195612",
                     Sex = EnumSex.Male,
-                    BirthDate = new DateTime(1999, 3, 3),
+                    BirthDate = new DateTime(1945, 1, 1),
                     IsActive = true,
                     LastVisitDate = null
                 };
 
                 Patient.AddPatient(patient, testContext);
-                
+
 
             }
 		}
