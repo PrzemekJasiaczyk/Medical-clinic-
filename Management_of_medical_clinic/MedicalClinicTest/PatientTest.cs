45267c22c6202bced1da00794dfc3a4a539f8b36
Natalia Hańczka <UL0269767@edu.uni.lodz.pl>
2023-04-16 15:31:05 +0200

    Rename test FindPatientInDataBase -> FindExistingPatientInDataBase

--- Management_of_medical_clinic/MedicalClinicTest/PatientTest.cs
+++ Management_of_medical_clinic/MedicalClinicTest/PatientTest.cs
@@ -6,33 +6,39 @@ namespace MedicalClinicTest
 {
 	public class PatientTest
 	{
-		// Natalia: dodawanie, znajdywanie pacjenta
-
 		[Fact]
-
-		public void AddPatientToDataBase()
+		public void FindExistingPatientInDataBase()
 		{
-			AppDbContext testContext;
 
-			using (InMemoryDbContext inMemoryDbContext = new())
-			{
-                testContext = inMemoryDbContext.CreateTestContext();
-                Patient patient =
-                new Patient()
-                {
-                    FirstName = "Paweł",
-                    LastName = "Dawid",
-                    PESEL = "45010195612",
-                    Sex = EnumSex.Male,
-                    BirthDate = new DateTime(1945, 1, 1),
-                    IsActive = true,
-                    LastVisitDate = null
-                };
-
-                Patient.AddPatient(patient, testContext);
-
-
-            }
 		}
+
+
+
+		//[Fact]
+
+		//public void AddPatientToDataBase()
+		//{
+		//	AppDbContext testContext;
+
+		//	using (InMemoryDbContext inMemoryDbContext = new())
+		//	{
+  //              testContext = inMemoryDbContext.CreateTestContext();
+  //              Patient patient =
+  //              new Patient()
+  //              {
+  //                  FirstName = "Paweł",
+  //                  LastName = "Dawid",
+  //                  PESEL = "45010195612",
+  //                  Sex = EnumSex.Male,
+  //                  BirthDate = new DateTime(1945, 1, 1),
+  //                  IsActive = true,
+  //                  LastVisitDate = null
+  //              };
+
+  //              Patient.AddPatient(patient, testContext);
+
+
+  //          }
+//			}
     }
 }
