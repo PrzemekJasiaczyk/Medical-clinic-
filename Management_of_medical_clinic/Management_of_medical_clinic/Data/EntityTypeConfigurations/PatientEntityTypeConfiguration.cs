e72f19b5192ca4dceacaec733563d80afe878a6d
mabrowarski <mabrowarski@gmail.com>
2023-04-18 20:43:06 +0200

    Add more Patient type records to database

--- Management_of_medical_clinic/Management_of_medical_clinic/Data/EntityTypeConfigurations/PatientEntityTypeConfiguration.cs
+++ Management_of_medical_clinic/Management_of_medical_clinic/Data/EntityTypeConfigurations/PatientEntityTypeConfiguration.cs
@@ -74,9 +74,9 @@ namespace Console_Management_of_medical_clinic.Data.EntityTypeConfigurations
                     new Patient
                     {
                         PatientId = 1,
-                        FirstName = "John",
-                        LastName = "Doe",
-                        PESEL = "12345678901",
+                        FirstName = "Juan Pablo",
+                        LastName = "Rodriguez",
+                        PESEL = "90010100191",
                         Sex = EnumSex.Male,
                         BirthDate = new DateTime(1990, 1, 1).Date,
                         IsActive = true
@@ -84,14 +84,43 @@ namespace Console_Management_of_medical_clinic.Data.EntityTypeConfigurations
                     new Patient
                     {
                         PatientId = 2,
-                        FirstName = "Jane",
-                        LastName = "Doe",
-                        PESEL = "23456789012",
+                        FirstName = "Janina",
+                        LastName = "Pumpernikiel",
+                        PESEL = "95020200222",
                         Sex = EnumSex.Female,
                         BirthDate = new DateTime(1995, 2, 2).Date,
                         IsActive = true
+                    },
+                    new Patient
+                    {
+                        PatientId = 3,
+                        FirstName = "Aleksander I",
+                        LastName = "Romanow",
+                        PESEL = "70120300913",
+                        Sex = EnumSex.Male,
+                        BirthDate = new DateTime(1970, 12, 3).Date,
+                        IsActive = true
+                    },
+                    new Patient
+                    {
+                        PatientId = 4,
+                        FirstName = "Żaneta",
+                        LastName = "Skowron-Ćwir",
+                        PESEL = "50120300123",
+                        Sex = EnumSex.Female,
+                        BirthDate = new DateTime(1950, 12, 3).Date,
+                        IsActive = true
+                    },
+                    new Patient
+                    {
+                        PatientId = 5,
+                        FirstName = "Henryk",
+                        LastName = "Walezy",
+                        PESEL = "00320300213",
+                        Sex = EnumSex.Male,
+                        BirthDate = new DateTime(2000, 12, 3).Date,
+                        IsActive= true
                     }
-
                 );
         }
     }
