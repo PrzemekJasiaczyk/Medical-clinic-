de9bf1b55ada5f1248ee08c739dd614d281efc43
mabrowarski <mabrowarski@gmail.com>
2023-04-18 19:49:53 +0200

    Write unit test for GetPatientsData() in PatientService class

--- Management_of_medical_clinic/MedicalClinicTest/PatientServiceTest.cs
+++ Management_of_medical_clinic/MedicalClinicTest/PatientServiceTest.cs
@@ -1,11 +1,55 @@
 using Console_Management_of_medical_clinic.Logic;
 using Console_Management_of_medical_clinic.Model;
 using Console_Management_of_medical_clinic.Data.Enums;
+using Console_Management_of_medical_clinic.Data;
 
 namespace MedicalClinicTest
 {
 	public class PatientServiceTest
 	{
+
+		[Fact]
+		public void GetPatients_ExpectedResults()
+		{
+			AppDbContext dbContext = new();
+			PatientService patientService = new();
+
+			Patient firstPatient = new()
+			{
+				FirstName = "Pawe³",
+				LastName = "Dawid",
+				PESEL = "45010195612",
+				Sex = EnumSex.Male,
+				BirthDate = new DateTime(1945, 1, 1),
+				IsActive = true,
+				LastVisitDate = null
+			};
+
+			Patient secondPatient = new()
+			{
+				FirstName = "Jan",
+				LastName = "Wiktorowicz",
+				PESEL = "99030300191",
+				Sex = EnumSex.Male,
+				BirthDate = new DateTime(1999, 3, 3),
+				IsActive = true,
+				LastVisitDate = null
+			};
+
+			dbContext.Patients.AddRange(firstPatient, secondPatient);
+			dbContext.SaveChanges();
+
+			var result = patientService.GetPatientData();
+
+			Assert.NotNull(result);
+			Assert.True(result.Count >= 2);
+			Assert.Contains(result, p => p.PESEL == "45010195612");
+			Assert.Contains(result, p => p.PESEL == "99030300191");
+
+			dbContext.Patients.RemoveRange(firstPatient, secondPatient);
+			dbContext.SaveChanges();
+		}
+
 		[Fact]
 		public void NullNameTest()
 		{
