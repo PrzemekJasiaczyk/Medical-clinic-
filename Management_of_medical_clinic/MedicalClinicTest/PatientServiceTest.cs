9548b306f5341ae91df315329ce6717db5dfa09b
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2023-04-18 21:55:49 +0200

    Add unit tests for PESEL number validation

--- Management_of_medical_clinic/MedicalClinicTest/PatientServiceTest.cs
+++ Management_of_medical_clinic/MedicalClinicTest/PatientServiceTest.cs
@@ -173,5 +173,274 @@ namespace MedicalClinicTest
 			dbContext.Patients.RemoveRange(_firstFakePatient, _secondFakePatient);
 			dbContext.SaveChanges();
 		}
-	}
+
+        [Fact]
+        public void IsValidPESEL_TooShort()
+        {
+            string errorMessage;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "9001011234",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL, patient.BirthDate,
+                patient.Sex, out errorMessage);
+
+            Assert.False(result);
+            Assert.True(errorMessage == "Given PESEL is too short. It should have 11 digits.");
+
+        }
+        [Fact]
+        public void IsValidPESEL_TooLong()
+        {
+            string errorMessage;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "9001011234599",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL,
+                patient.BirthDate, patient.Sex, out errorMessage);
+
+            Assert.False(result);
+            Assert.True(errorMessage == "Given PESEL is too long. It should have 11 digits.");
+
+        }
+        [Fact]
+        public void IsValidPESEL_Correct()
+        {
+            string errorMessage = string.Empty;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "90010112335",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL,
+                patient.BirthDate, patient.Sex, out errorMessage);
+
+
+            Assert.True(result);
+            Assert.Equal("", errorMessage);
+        }
+
+        [Fact]
+        public void IsValidPESEL_Empty()
+        {
+            string errorMessage;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL,
+                patient.BirthDate, patient.Sex, out errorMessage);
+
+
+            Assert.True(errorMessage == "PESEL cannot be empty. It should 11 digits.");
+            Assert.False(result);
+        }
+        [Fact]
+        public void IsValidPESEL_letters()
+        {
+            string errorMessage;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "900101a2345",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL,
+                patient.BirthDate, patient.Sex, out errorMessage);
+
+
+            Assert.True(errorMessage == "PESEL cannot contain letters or special characters");
+            Assert.False(result);
+        }
+        [Fact]
+        public void IsValidPESEL_SpecialCharacters()
+        {
+            string errorMessage;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "900101@2345",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL,
+                patient.BirthDate, patient.Sex, out errorMessage);
+
+
+            Assert.True(errorMessage == "PESEL cannot contain letters or special characters");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidPESEL_notMatch()
+        {
+            string errorMessage;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "90010112345",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 2, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL,
+                patient.BirthDate, patient.Sex, out errorMessage);
+
+
+            Assert.True(errorMessage == "PESEL has incorrect birth date");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValidPESEL_incorrectGenderMale()
+        {
+            string errorMessage;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "90010112245",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL,
+                patient.BirthDate, patient.Sex, out errorMessage);
+
+
+            Assert.True(errorMessage == "PESEL has incorrect gender. For men it's an odd number.");
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void IsValidPESEL_incorrectGenderFemale()
+        {
+            string errorMessage;
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Anna",
+                LastName = "Nowak",
+                PESEL = "90020201578",
+                Sex = EnumSex.Female,
+                BirthDate = new DateTime(1990, 2, 2),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+
+            bool result = patientService.IsValidPESEL(patient.PESEL,
+                patient.BirthDate, patient.Sex, out errorMessage);
+
+
+            Assert.True(errorMessage == "PESEL has incorrect gender. For women it's 0 or even number");
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void IsValidPESEL_unique()
+        {
+            string errorMessage;
+            var dbContext = new AppDbContext();
+            PatientService patientService = new();
+            Patient patient = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "90010112335",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+
+            Patient.AddPatient(patient, dbContext);
+            dbContext.SaveChanges();
+            Patient patient2 = new Patient()
+            {
+                FirstName = "Jan",
+                LastName = "Nowak",
+                PESEL = "90010112335",
+                Sex = EnumSex.Male,
+                BirthDate = new DateTime(1990, 1, 1),
+                IsActive = true,
+                LastVisitDate = null
+            };
+            bool result = patientService.IsValidPESEL(patient2.PESEL,
+                patient2.BirthDate, patient2.Sex, out errorMessage);
+
+            Assert.False(result);
+            Assert.True(errorMessage == "PESEL is not unique. Patient with given PESEL already exist");
+
+            dbContext.Patients.Remove(patient);
+            dbContext.SaveChanges();
+        }
+    }
+
+
+
+
 }
+        
+
+	
