828130c48c67e2b80d90bcb10cb655fc31b331e2
paulinaswaa <124894941+paulinaswaa@users.noreply.github.com>
2023-04-29 14:33:28 +0200

    Add FormRegisterAppointment

---
+++ Management_of_medical_clinic/GUI_Management_of_medical_clinic/FormRegisterAppointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Management_of_medical_clinic
+{
+    public partial class FormRegisterAppointment : Form
+    {
+        public FormRegisterAppointment()
+        {
+            InitializeComponent();
+        }
+    }
+}
