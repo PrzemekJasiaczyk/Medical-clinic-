8ed14fe0c3be94536e6527b514e278f0837b6e13
Kamila Zych <UL0262211@edu.uni.lodz.pl>
2023-05-06 21:26:29 +0200

    Add unit tests for the GetPatientDataByIdPatient method

--- Management_of_medical_clinic/MedicalClinicTest/CalendarAppointmentServiceTest.cs
+++ Management_of_medical_clinic/MedicalClinicTest/CalendarAppointmentServiceTest.cs
@@ -1,12 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Console_Management_of_medical_clinic.Logic;
+using Console_Management_of_medical_clinic.Model;
+using Console_Management_of_medical_clinic.Data.Enums;
+using Console_Management_of_medical_clinic.Data;
 
 namespace MedicalClinicTest
 {
-    internal class CalendarAppointmentServiceTest
+    public class CalendarAppointmentServiceTest
     {
+        #region Fake Data
+        Patient fakePatient = new Patient()
+        {
+            PatientId = 1000,
+            FirstName = "Jan",
+            LastName = "Kowalski",
+            PESEL = "88010112339",
+            Sex = EnumSex.Male,
+            BirthDate = new DateTime(1988, 1, 1),
+            IsActive = true,
+            LastVisitDate = null
+        };
+
+        EmployeeModel fakeEmployee = new EmployeeModel()
+        {
+            IdEmployee = 1000,
+            FirstName = "Piotr",
+            LastName = "Nowak",
+            PESEL= "92013012345",
+            DateOfBirth="13-01-1992"
+        };
+
+        AppointmentModel firstFakeAppointment = new AppointmentModel()
+        {
+            IdTerm = 1,
+            IdDay = 11,
+            Cost = 1000,
+            IsActive = false,
+            IdCalendar = 1,
+            IdEmployee = 1000,
+            PatientId = 1000,
+            IdOffice = 1
+        };
+
+        AppointmentModel secondFakeAppointment = new AppointmentModel()
+        {
+            IdTerm = 1,
+            IdDay = 11,
+            Cost = 1000,
+            IsActive = true,
+            IdCalendar = 1,
+            IdEmployee = 1000,
+            PatientId = null,
+            IdOffice = 1
+        };
+
+        #endregion
+
+        #region GetPatientDataByIdPatient
+
+        [Fact]
+        public void TestGetPatientDataByIdPatient_Correct()
+        {
+            AppDbContext context = new AppDbContext();
+
+            Patient patient = fakePatient;
+            context.Patients.Add(patient);
+            context.SaveChanges();
+
+            EmployeeModel employee = fakeEmployee;
+            context.DbEmployees.Add(employee);
+            context.SaveChanges();
+
+            AppointmentModel first = firstFakeAppointment; 
+            context.DbAppointments.Add(first);
+            context.SaveChanges();
+
+            Patient result = CalendarAppointmentService.GetPatientDataByIdPatient(first);
+
+            Assert.NotNull(result);
+            Assert.Equal(patient.FirstName, result.FirstName);
+            Assert.Equal(patient.LastName, result.LastName);
+            Assert.Equal(patient.PESEL, result.PESEL);
+            Assert.Equal(patient.Sex, result.Sex);
+            Assert.Equal(patient.BirthDate, result.BirthDate);
+            Assert.Equal(patient.IsActive, result.IsActive);
+            Assert.Equal(patient.LastVisitDate, result.LastVisitDate);
+
+            context.DbAppointments.Remove(first);
+            context.DbEmployees.Remove(employee);
+            context.Patients.Remove(patient);
+            context.SaveChanges();
+        }
+
+        [Fact]
+        public void GetPatientDataByIdPatient_PatientNotFound()
+        {
+            AppointmentModel appointment = new AppointmentModel
+            {
+                PatientId = 9999
+            };
+
+            Exception exception = Assert.Throws<Exception>(() => CalendarAppointmentService.GetPatientDataByIdPatient(appointment));
+            Assert.Equal("Patient not found in database", exception.Message);
+        }
+
+        [Fact]
+        public void GetPatientDataByIdPatient_PatientIsNull()
+        {
+            AppointmentModel appointment = new AppointmentModel
+            {
+                PatientId = null
+            };
+
+            Exception exception = Assert.Throws<Exception>(() => CalendarAppointmentService.GetPatientDataByIdPatient(appointment));
+            Assert.Equal("PatientId = null", exception.Message);
+        }
+
+        #endregion
+
     }
 }
