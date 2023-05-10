b9864c4c96dd52868bd0236363752fcc0d2e26b8
mabrowarski <mabrowarski@gmail.com>
2023-05-10 21:51:06 +0200

    Add unit tests for filter method in CalendarService class

--- Management_of_medical_clinic/MedicalClinicTest/CalendarServiceTest.cs
+++ Management_of_medical_clinic/MedicalClinicTest/CalendarServiceTest.cs
@@ -44,15 +44,89 @@ namespace MedicalClinicTest
 		}
 
 		[Fact]
-		public void Filter_Correct()
+		public void Filter_ExistingCalendar()
 		{
+			string dataReference = "03";
+			string activityStatus = "Inactive";
+			CalendarService calendarService = new();
 
+			List<CalendarModel> filteredCalendars = calendarService.Filter(dataReference, activityStatus);
+			bool containsCalendar = filteredCalendars.Any(c => c.DateReference.StartsWith(dataReference) && c.Active == false);
+
+			Assert.True(containsCalendar);
 		}
 
 		[Fact]
-		public void Filter_Incorrect() 
+		public void Filter_NotExistingCalendar()
 		{
+			string dataReference = "02-2023";
+			string activityStatus = "Active";
+			CalendarService calendarService = new();
 
+			List<CalendarModel> filteredCalendars = calendarService.Filter(dataReference, activityStatus);
+			bool containsCalendar = filteredCalendars.Any(c => c.DateReference.StartsWith(dataReference) && c.Active == true);
+
+			Assert.False(containsCalendar);
+		}
+
+		[Fact]
+		public void Filter_AllActive_Correct()
+		{
+			string activityStatus = "Active";
+			CalendarService calendarService = new();
+
+			List<CalendarModel> filteredCalendars = calendarService.Filter("", activityStatus);
+			int filteredCount = filteredCalendars.Count();
+
+			Assert.Equal(2, filteredCount);
+		}
+
+		[Fact]
+		public void Filter_AllActive_Incorrect()
+		{
+			string activityStatus = "Active";
+			CalendarService calendarService = new();
+
+			List<CalendarModel> filteredCalendars = calendarService.Filter("", activityStatus);
+			int filteredCount = filteredCalendars.Count();
+
+			Assert.NotEqual(0, filteredCount);
+		}
+
+		[Fact]
+		public void Filter_AllInactive_Correct()
+		{
+			string activityStatus = "Inactive";
+			CalendarService calendarService = new();
+
+			List<CalendarModel> filteredCalendars = calendarService.Filter("", activityStatus);
+			int filteredCount = filteredCalendars.Count();
+
+			Assert.Equal(1, filteredCount);
+		}
+
+		[Fact]
+		public void Filter_AllInactive_Incorrect()
+		{
+			string activityStatus = "Inactive";
+			CalendarService calendarService = new();
+
+			List<CalendarModel> filteredCalendars = calendarService.Filter("", activityStatus);
+			int filteredCount = filteredCalendars.Count();
+
+			Assert.NotEqual(0, filteredCount);
+		}
+
+		[Fact]
+		public void Filter_NotExisting()
+		{
+			string dataReference = "01-1900";
+			CalendarService calendarService = new();
+
+			List<CalendarModel> filteredCalendars = calendarService.Filter(dataReference, "");
+			int filteredCount = filteredCalendars.Count();
+
+			Assert.Equal(0, filteredCount);
 		}
 	}
 }
