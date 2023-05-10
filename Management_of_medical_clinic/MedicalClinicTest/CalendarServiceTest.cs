8e7f9d960a558c124f05ab52a27c687b9f371ec5
mabrowarski <mabrowarski@gmail.com>
2023-05-10 21:34:47 +0200

    Add unit tests to sorting calendars method in CalendarService

---
+++ Management_of_medical_clinic/MedicalClinicTest/CalendarServiceTest.cs
@@ -0,0 +1,58 @@
+﻿using Console_Management_of_medical_clinic.Logic;
+using Console_Management_of_medical_clinic.Model;
+
+namespace MedicalClinicTest
+{
+	public class CalendarServiceTest
+	{
+		[Fact]
+		public void SortAscending_Correct()
+		{
+			CalendarService calendarService = new();
+			List<CalendarModel> sortedCalendars = calendarService.Sort("", true);
+
+			Assert.StartsWith("03", sortedCalendars[0].DateReference);
+		}
+
+		[Fact]
+		public void SortDescending_Correct()
+		{
+			CalendarService calendarService = new();
+			List<CalendarModel> sortedCalendars = calendarService.Sort("", false);
+
+			Assert.StartsWith("05", sortedCalendars[0].DateReference);
+		}
+
+		[Fact]
+		public void SortAscending_Incorrect()
+		{
+			CalendarService calendarService = new();
+			List<CalendarModel> sortedCalendars = calendarService.Sort("", true);
+
+			bool incorrectFirstMonth = sortedCalendars[0].DateReference.StartsWith("05");
+			Assert.False(incorrectFirstMonth);
+		}
+
+		[Fact]
+		public void SortDescending_Incorrect()
+		{
+			CalendarService calendarService = new();
+			List<CalendarModel> sortedCalendars = calendarService.Sort("", false);
+
+			bool incorrectFirstMonth = sortedCalendars[0].DateReference.StartsWith("03");
+			Assert.False(incorrectFirstMonth);
+		}
+
+		[Fact]
+		public void Filter_Correct()
+		{
+
+		}
+
+		[Fact]
+		public void Filter_Incorrect() 
+		{
+
+		}
+	}
+}
