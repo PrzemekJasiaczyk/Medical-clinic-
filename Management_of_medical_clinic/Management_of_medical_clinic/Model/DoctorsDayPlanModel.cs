34138d51224b7b4a34f90d3129601f2f34cf08a6
mabrowarski <mabrowarski@gmail.com>
2023-05-17 21:26:10 +0200

    Modify DoctorsDayPlanModel to have a cost column

--- Management_of_medical_clinic/Management_of_medical_clinic/Model/DoctorsDayPlanModel.cs
+++ Management_of_medical_clinic/Management_of_medical_clinic/Model/DoctorsDayPlanModel.cs
@@ -14,15 +14,16 @@ namespace Console_Management_of_medical_clinic.Model
         public int IdOfTerm { get; set; }
         public int IdDay { get; set; }
         public bool IsActive { get; set; }
-        //Relationships
-        [ForeignKey("CalendarModel")] public int? IdCalendar { get; set; }
+        public decimal? Cost { get; set; } = 100;
+		//Relationships
+		[ForeignKey("CalendarModel")] public int? IdCalendar { get; set; }
         public CalendarModel CalendarModel { get; set; }
         [ForeignKey("EmployeeModel")] public int? IdEmployee { get; set; }
         public EmployeeModel EmployeeModel { get; set; }
         [ForeignKey("OfficeModel")] public int? IdOffice { get; set; }
         public OfficeModel OfficeModel { get; set; }
         [ForeignKey("Patient")] public int? PatientId { get; set; }
-        public Patient Patient { get; set; }
+        public Patient? Patient { get; set; }
 
 
         public DoctorsDayPlanModel() { }
@@ -66,5 +67,21 @@ namespace Console_Management_of_medical_clinic.Model
             IsActive = isActive;
         }
 
-    }
+		public override string ToString()
+		{
+			return CalendarService.GetDateByIdCalendar((int)IdCalendar, IdDay).ToShortDateString() + " " + AppointmentService.GetTermByTermId(IdTerm).ToString() + "  (Cost: " + Cost + ")";
+
+		}
+
+
+		public object[] appointmentData => new object[] {
+			PatientService.GetPatientById((int)PatientId).ToString(),
+			PatientService.GetPatientById((int)PatientId).PESEL,
+			EmployeeService.GetEmployeeByID((int)IdEmployee).ToString(),
+			SpecializationService.GetSpecializationById((int)EmployeeService.GetEmployeeByID((int)IdEmployee).IdSpecialization).Name,
+			CalendarService.GetDateByIdCalendar((int)IdCalendar,IdDay).ToShortDateString(),
+			OfficeService.GetOfficeById((int)IdOffice).Number,
+			Cost.ToString()
+		};
+	}
 }
