b90f39fa6919c8b98f6a5f5a16bfb7ca728e6f83
mabrowarski <mabrowarski@gmail.com>
2023-05-21 20:40:18 +0200

    Add class generating payment confirmation in form of PDF

---
+++ Management_of_medical_clinic/Management_of_medical_clinic/Logic/PaymentConfirmationGenerator.cs
@@ -0,0 +1,18 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using iText.IO.Image;
+using Console_Management_of_medical_clinic.Logic.Interfaces;
+using Console_Management_of_medical_clinic.Model;
+
+namespace Console_Management_of_medical_clinic.Logic
+{
+	public class PaymentConfirmationGenerator : IPaymentConfirmationGenerator
+	{
+		public void GenerateConfirmation(DoctorsDayPlanModel appointment)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
