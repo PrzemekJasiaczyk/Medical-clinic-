ef84ee672f64e7ab03352b1cf246851f860a53f1
MishaPuch <mpuczkowskij@gmail.com>
2023-05-23 12:00:12 +0200

    Revert "Add patient list to dashboard"
    This reverts commit 0ee37524a71dc7f6ebcf713a736fd9c3006ffdd5.

--- Management_of_medical_clinic/GUI_Management_of_medical_clinic/FormDoctorDashboard.Designer.cs
+++ Management_of_medical_clinic/GUI_Management_of_medical_clinic/FormDoctorDashboard.Designer.cs
@@ -35,31 +35,24 @@
             buttonCurrentCalendar = new Button();
             button2 = new Button();
             label1 = new Label();
-            dataGridView1 = new DataGridView();
-            labelName = new Label();
-            Labelpesel = new Label();
-            LabelbirthDay = new Label();
-            Labelsex = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // buttonLogOut
             // 
-            buttonLogOut.BackColor = Color.Gainsboro;
-            buttonLogOut.FlatAppearance.BorderColor = Color.White;
-            buttonLogOut.FlatAppearance.BorderSize = 2;
-            buttonLogOut.FlatStyle = FlatStyle.Flat;
-            buttonLogOut.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonLogOut.Location = new Point(33, 1293);
-            buttonLogOut.Margin = new Padding(3, 4, 3, 4);
-            buttonLogOut.Name = "buttonLogOut";
-            buttonLogOut.Size = new Size(288, 88);
-            buttonLogOut.TabIndex = 40;
-            buttonLogOut.Text = "LOG OUT";
-            buttonLogOut.UseVisualStyleBackColor = false;
-            buttonLogOut.Click += buttonLogOut_Click;
+            this.buttonLogOut.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonLogOut.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonLogOut.FlatAppearance.BorderSize = 2;
+            this.buttonLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLogOut.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonLogOut.Location = new System.Drawing.Point(29, 970);
+            this.buttonLogOut.Name = "buttonLogOut";
+            this.buttonLogOut.Size = new System.Drawing.Size(252, 66);
+            this.buttonLogOut.TabIndex = 40;
+            this.buttonLogOut.Text = "LOG OUT";
+            this.buttonLogOut.UseVisualStyleBackColor = false;
+            this.buttonLogOut.Click += new System.EventHandler(this.buttonLogOut_Click);
             // 
             // panel1
             // 
@@ -70,19 +63,17 @@
             panel1.Controls.Add(buttonCurrentCalendar);
             panel1.Controls.Add(button2);
             panel1.Location = new Point(0, 0);
-            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(357, 1441);
+            panel1.Size = new Size(312, 1081);
             panel1.TabIndex = 39;
             // 
             // pictureBox1
             // 
             pictureBox1.BackgroundImage = Properties.Resources.MC_Logo;
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Location = new Point(70, 16);
-            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Location = new Point(61, 12);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(201, 224);
+            pictureBox1.Size = new Size(176, 168);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
@@ -92,10 +83,9 @@
             buttonCalendar.FlatStyle = FlatStyle.Flat;
             buttonCalendar.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
             buttonCalendar.ForeColor = SystemColors.ActiveCaptionText;
-            buttonCalendar.Location = new Point(-8, 345);
-            buttonCalendar.Margin = new Padding(3, 4, 3, 4);
+            buttonCalendar.Location = new Point(-7, 259);
             buttonCalendar.Name = "buttonCalendar";
-            buttonCalendar.Size = new Size(376, 116);
+            buttonCalendar.Size = new Size(329, 87);
             buttonCalendar.TabIndex = 27;
             buttonCalendar.Text = "Calendar";
             buttonCalendar.UseVisualStyleBackColor = false;
@@ -107,10 +97,9 @@
             buttonCurrentCalendar.FlatStyle = FlatStyle.Flat;
             buttonCurrentCalendar.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
             buttonCurrentCalendar.ForeColor = SystemColors.ActiveCaptionText;
-            buttonCurrentCalendar.Location = new Point(-8, 469);
-            buttonCurrentCalendar.Margin = new Padding(3, 4, 3, 4);
+            buttonCurrentCalendar.Location = new Point(-7, 352);
             buttonCurrentCalendar.Name = "buttonCurrentCalendar";
-            buttonCurrentCalendar.Size = new Size(376, 116);
+            buttonCurrentCalendar.Size = new Size(329, 87);
             buttonCurrentCalendar.TabIndex = 33;
             buttonCurrentCalendar.Text = "Current calendar";
             buttonCurrentCalendar.UseVisualStyleBackColor = false;
@@ -121,96 +110,38 @@
             button2.BackColor = Color.SteelBlue;
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.Location = new Point(-8, 593);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(-7, 445);
             button2.Name = "button2";
-            button2.Size = new Size(376, 116);
+            button2.Size = new Size(329, 87);
             button2.TabIndex = 28;
             button2.Text = "[..]";
             button2.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(423, 16);
-            label1.Name = "label1";
-            label1.Size = new Size(527, 81);
-            label1.TabIndex = 40;
-            label1.Text = "Welcome message";
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(423, 124);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(578, 272);
-            dataGridView1.TabIndex = 41;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-            // 
-            // labelName
-            // 
-            labelName.AutoSize = true;
-            labelName.Location = new Point(1047, 124);
-            labelName.Name = "labelName";
-            labelName.Size = new Size(83, 20);
-            labelName.TabIndex = 42;
-            labelName.Text = "Full Name :";
-            labelName.Click += label2_Click;
-            // 
-            // Labelpesel
-            // 
-            Labelpesel.AutoSize = true;
-            Labelpesel.Location = new Point(1047, 177);
-            Labelpesel.Name = "Labelpesel";
-            Labelpesel.Size = new Size(55, 20);
-            Labelpesel.TabIndex = 43;
-            Labelpesel.Text = "PESEL :";
-            // 
-            // LabelbirthDay
-            // 
-            LabelbirthDay.AutoSize = true;
-            LabelbirthDay.Location = new Point(1047, 284);
-            LabelbirthDay.Name = "LabelbirthDay";
-            LabelbirthDay.Size = new Size(77, 20);
-            LabelbirthDay.TabIndex = 44;
-            LabelbirthDay.Text = "Birth Day :";
-            // 
-            // Labelsex
-            // 
-            Labelsex.AutoSize = true;
-            Labelsex.Location = new Point(1047, 231);
-            Labelsex.Name = "Labelsex";
-            Labelsex.Size = new Size(39, 20);
-            Labelsex.TabIndex = 45;
-            Labelsex.Text = "Sex :";
-            Labelsex.Click += label5_Click;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(370, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(420, 65);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "Welcome message";
             // 
             // FormDoctorDashboard
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
-            ClientSize = new Size(1942, 1102);
-            Controls.Add(Labelsex);
-            Controls.Add(LabelbirthDay);
-            Controls.Add(Labelpesel);
-            Controls.Add(labelName);
-            Controls.Add(dataGridView1);
+            ClientSize = new Size(1920, 1080);
             Controls.Add(label1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "FormDoctorDashboard";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "FormOfficeList";
             WindowState = FormWindowState.Maximized;
-            Load += FormDoctorDashboard_Load;
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -223,10 +154,5 @@
         private Button buttonCurrentCalendar;
         private Button button2;
         private Label label1;
-        private DataGridView dataGridView1;
-        private Label labelName;
-        private Label Labelpesel;
-        private Label LabelbirthDay;
-        private Label Labelsex;
     }
 }
--- Management_of_medical_clinic/GUI_Management_of_medical_clinic/FormDoctorDashboard.cs
+++ Management_of_medical_clinic/GUI_Management_of_medical_clinic/FormDoctorDashboard.cs
@@ -1,5 +1,4 @@
-﻿using Console_Management_of_medical_clinic.Data;
-using Console_Management_of_medical_clinic.Logic;
+﻿using Console_Management_of_medical_clinic.Logic;
 using Console_Management_of_medical_clinic.Model;
 using Microsoft.VisualBasic;
 using System;
@@ -17,7 +16,6 @@ namespace GUI_Management_of_medical_clinic
     public partial class FormDoctorDashboard : Form
     {
         EmployeeModel currentUser;
-        Patient? currentPatient;
 
         public FormDoctorDashboard()
         {
@@ -36,12 +34,12 @@ namespace GUI_Management_of_medical_clinic
             bool isNewCalendar = true;
             //bool isNewCalendar = false; roboczo dałem true
             List<CalendarModel> calendars = CalendarService.GetCalendarData();
-
+            
             foreach (CalendarModel calendar in calendars)
-            {
+            {                
                 if (calendar.IdEmployee == currentUser.IdEmployee && calendar.Active == false)
                 {
-                    isNewCalendar = true;
+                    isNewCalendar = true;        
                 }
             }
             if (isNewCalendar == true)
@@ -53,8 +51,8 @@ namespace GUI_Management_of_medical_clinic
             else
             {
                 MessageBox.Show("You don't have any new calendar to check", "Information", MessageBoxButtons.OK);
-            }
-
+            }           
+            
         }
 
         private void buttonLogOut_Click(object sender, EventArgs e)
@@ -80,8 +78,8 @@ namespace GUI_Management_of_medical_clinic
             List<CalendarModel> calendars = CalendarService.GetCalendarData();
             foreach (CalendarModel calendar in calendars)
             {
-                if (calendar.IdEmployee == currentUser.IdEmployee &&
-                    calendar.Active == true &&
+                if (calendar.IdEmployee == currentUser.IdEmployee && 
+                    calendar.Active == true && 
                     calendar.DateReference == currentMonthYear)
                 {
                     isCurrentCalendar = true;
@@ -97,7 +95,7 @@ namespace GUI_Management_of_medical_clinic
             {
                 MessageBox.Show("Brak takich");
             }
-
+            
         }
         private void ToFormCurrentCalendar()
         {
@@ -105,66 +103,6 @@ namespace GUI_Management_of_medical_clinic
             this.Hide();
             formCurrentCalendar.ShowDialog();
             this.Close();
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            AppDbContext db = new AppDbContext();
-
-            if (e.RowIndex >= 0)
-            {
-                string? row = dataGridView1.Rows[e.RowIndex].Cells["PatientId"].Value.ToString();
-                currentPatient = db.Patients.FirstOrDefault(p => p.PatientId == Convert.ToUInt32(row));
-
-                labelName.Text = $"Full Name : {currentPatient.FirstName} {currentPatient.LastName}";
-                Labelpesel.Text = $"PESEL : {currentPatient.PESEL}";
-                Labelsex.Text = $"Sex : {currentPatient.Sex}";
-                LabelbirthDay.Text = $"Birth Day : {Convert.ToDateTime(currentPatient.BirthDate)}";
-            }
-        }
-
-        private void FormDoctorDashboard_Load(object sender, EventArgs e)
-        {
-            dataGridView1.Rows.Clear();
-            dataGridView1.Columns.Add("Room", "Room");
-            dataGridView1.Columns.Add("Hour", "Hour");
-            dataGridView1.Columns.Add("Patient", "Patient");
-            dataGridView1.Columns.Add("Day", "Day");
-            dataGridView1.Columns.Add("PatientId", "PatientId");
-
-
-
-            AppDbContext db = new AppDbContext();
-
-            List<DoctorsDayPlanModel> appointments = db.DbDoctorsDayPlan.ToList();
-            foreach (DoctorsDayPlanModel appointment in appointments)
-            {
-                if (appointment.IdEmployee == currentUser.IdEmployee &&  /*appointment.IdDay==DateTime.Now.Day &&*/  appointment.IdCalendar == 1)
-                {
-                    string timeTerm = AppointmentService.GetTermByTermId(appointment.IdOfTerm);
-
-                    if (db.Patients.FirstOrDefault(p => p.PatientId == appointment.PatientId) != null)
-                    {
-
-
-                        Patient? patient = db.Patients.FirstOrDefault(p => p.PatientId == appointment.PatientId);
-                        int index = dataGridView1.Rows.Add(appointment.IdOffice, timeTerm, patient.LastName, appointment.IdDay, patient.PatientId);
-                    }
-                }
-            }
-
-            dataGridView1.CellDoubleClick += dataGridView1_CellContentClick;
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
