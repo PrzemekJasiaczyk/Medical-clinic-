03814338f2834df04c06e8d31d9307b449b896ab
MichalJ23 <ul0260196@edu.uni.lodz.pl>
2023-06-01 15:56:32 +0200

    Unit tests for Employee

---
+++ Management_of_medical_clinic/MedicalClinicTest/EmployeeServiceTest.cs
@@ -0,0 +1,137 @@
+﻿using Console_Management_of_medical_clinic.Data.Enums;
+using Console_Management_of_medical_clinic.Logic;
+using Console_Management_of_medical_clinic.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalClinicTest
+{
+    public class EmployeeServiceTest
+    {
+
+        [Fact]
+        public void TestGetEmployeesData()
+        {
+            // Act
+            List<EmployeeModel> result = EmployeeService.GetEmployeesData();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public void TestGetDoctors()
+        {
+            // Act
+            List<EmployeeModel> result = EmployeeService.GetDoctors();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.All(result, employee =>
+            {
+                Assert.Equal(EnumEmployeeRoles.MedicalDoctor, employee.Role);
+            });
+        }
+
+        [Fact]
+        public void TestValidateEmail_ValidInput()
+        {
+            // Arrange
+            string input = "john.doe@example.com";
+            string name = "john";
+
+            // Act
+            (string message, bool isValid) = EmployeeService.validateEmail(input, name);
+
+            // Assert
+            Assert.Equal("Success", message);
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void TestValidateEmail_InvalidInput()
+        {
+            // Arrange
+            string input = "invalid_email";
+            string name = "john";
+
+            // Act
+            (string message, bool isValid) = EmployeeService.validateEmail(input, name);
+
+            // Assert
+            Assert.NotEqual("Success", message);
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void TestGetEmployeeByID_ExistingEmployeeID()
+        {
+            // Arrange
+            int employeeID = 1;
+
+            // Act
+            EmployeeModel result = EmployeeService.GetEmployeeByID(employeeID);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(employeeID, result.IdEmployee);
+        }
+
+        [Fact]
+        public void TestGetEmployeeByID_NonExistingEmployeeID()
+        {
+            // Arrange
+            int employeeID = 999;
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => EmployeeService.GetEmployeeByID(employeeID));
+        }
+
+        [Fact]
+        public void TestValidatePhone_ValidInput()
+        {
+            // Arrange
+            string input = "123456789";
+
+            // Act
+            (string message, bool isValid) = EmployeeService.validatePhone(input);
+
+            // Assert
+            Assert.Equal("Success", message);
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void TestValidatePhone_InvalidInpute()
+        {
+            // Arrange
+            string input = "1234";
+
+            // Act
+            (string message, bool isValid) = EmployeeService.validatePhone(input);
+
+            // Assert
+            Assert.NotEqual("Success", message);
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void GetEmployeeInfoById()
+        {
+            // Arrange
+            int id = 1;
+            string expectedInfo = "Joe Doe";
+
+            // Act
+            string result = EmployeeService.GetEmployeeInfoById(id);
+
+            // Assert
+            Assert.Equal(expectedInfo, result);
+        }
+    }
+}
---
+++ Management_of_medical_clinic/MedicalClinicTest/EmployeeTest.cs
@@ -0,0 +1,83 @@
+﻿using Console_Management_of_medical_clinic.Data.Enums;
+using Console_Management_of_medical_clinic.Logic;
+using Console_Management_of_medical_clinic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalClinicTest
+{
+    public class EmployeeTest
+    {
+
+        [Fact]
+        public void TestFindEmployeeById()
+        {
+            // Arrange
+            int existingEmployeeId = 1;
+
+            // Act
+            EmployeeModel result = EmployeeModel.FindEmployee(existingEmployeeId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(existingEmployeeId, result.IdEmployee);
+        }
+
+        [Fact]
+        public void TestFilterEmployees_MatchingRoleAndActiveStatus()
+        {
+            // Arrange
+            EnumEmployeeRoles role = EnumEmployeeRoles.Janitor;
+            bool isActive = true;
+
+            // Act
+            List<EmployeeModel> result = EmployeeModel.FilterEmployees(role, isActive);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.All(result, employee =>
+            {
+                Assert.Equal(role, employee.Role);
+                Assert.Equal(isActive, employee.IsActive);
+            });
+        }
+
+        [Fact]
+        public void TestToString_FullName()
+        {
+            // Arrange
+            EmployeeModel employee = new EmployeeModel
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            // Act
+            string result = employee.ToString();
+
+            // Assert
+            Assert.Equal("Doe John", result);
+        }
+
+        [Fact]
+        public void TestFullNameWithId()
+        {
+            // Arrange
+            EmployeeModel employee = new EmployeeModel
+            {
+                IdEmployee = 1,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            // Act
+            string result = employee.FullNameWithId();
+
+            // Assert
+            Assert.Equal("1-John Doe", result);
+        }
+    }
+}
